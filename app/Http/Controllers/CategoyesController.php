ee86686cc541ae2b58ae77cb2ff39ad85bd06251
Ikram-ann-dev1 <roroannanne@gmail.com>
2022-04-12 20:58:23 +0100

    first commit

---
+++ app/Http/Controllers/CategoyesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\categoyes;
+use Illuminate\Http\Request;
+
+class CategoyesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\categoyes  $categoyes
+     * @return \Illuminate\Http\Response
+     */
+    public function show(categoyes $categoyes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\categoyes  $categoyes
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(categoyes $categoyes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\categoyes  $categoyes
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, categoyes $categoyes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\categoyes  $categoyes
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(categoyes $categoyes)
+    {
+        //
+    }
+}
---
+++ database/factories/CategoyesFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\categoyes;
+use Faker\Generator as Faker;
+
+$factory->define(categoyes::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
